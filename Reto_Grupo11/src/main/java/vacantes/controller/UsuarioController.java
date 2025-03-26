03d1163f7beefd84687b573cfb0dc1171002bc0a
Alberto <albertosantosgaldiano@gmail.com>
2025-03-26 19:40:05 +0100

    feat(usuario): CRUD Usuario e intento properties al gitignore

--- Reto_Grupo11/src/main/java/vacantes/controller/UsuarioController.java
+++ Reto_Grupo11/src/main/java/vacantes/controller/UsuarioController.java
@@ -1,10 +1,103 @@
 package vacantes.controller;
 
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vacantes.modelo.entities.Rol;
+import vacantes.modelo.entities.Usuario;
+import vacantes.modelo.services.UsuarioService;
 
 @RestController
 @CrossOrigin(origins = "*")
+
+@RequestMapping("/usuario")
+
 public class UsuarioController {
+	
+	@Autowired
+	UsuarioService usuarioService;
+	
+	/***** CRUD *****/
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> todos() {
+
+		return new ResponseEntity<List<Usuario>>(usuarioService.buscarTodos(), HttpStatus.OK);
+		
+		// POSTMAN: localhost:8445/usuario/all
+	}
+	
+	@GetMapping("/detail/{email}")
+	public ResponseEntity<?> uno(@PathVariable String email) {
+		Usuario usuario = usuarioService.buscarUno(email);
+		if (usuario != null)
+			return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+		else
+			return new ResponseEntity<String>("Este usuario no existe", HttpStatus.NOT_FOUND);
+		
+		// POSTMAN: localhost:8445/usuario/detail/cliente1@correo.com
+	}
+
+	@PutMapping("/edit/{email}")
+	public ResponseEntity<?> modificar(@PathVariable String email, @RequestBody Usuario usuarioActualizado) {
+		Usuario usuarioExistente = usuarioService.buscarUno(email);
+		if (usuarioExistente != null) {
+			usuarioActualizado.setEmail(email); // Asegurar el email no cambia.
+			switch(usuarioService.updateUno(usuarioActualizado)) {
+				case 1:  return new ResponseEntity<>("Usuario actualizado correctamente", HttpStatus.OK);
+				case 0:  return new ResponseEntity<>("Usuario no existe", HttpStatus.NOT_FOUND);
+				case -1: return new ResponseEntity<>("Esto es un problema de la base de datos, llame a servicio Tecnico", HttpStatus.BAD_REQUEST);
+				default:  return new ResponseEntity<>("Error desconocido", HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}	
+		else
+			return new ResponseEntity<String>("Este usuario no existe", HttpStatus.NOT_FOUND);
+		
+		// POSTMAN: localhost:8445/usuario/edit/cliente1@correo.com
+				
+	}
+
+
+	@PostMapping("/add")
+	public ResponseEntity<?> alta(@RequestBody Usuario usuario) {
+
+		usuario.setRol(Rol.ADMON);
+		
+	    Usuario nuevoUsuario = usuarioService.insertUno(usuario);
+
+	    if (nuevoUsuario != null) {
+	        return new ResponseEntity<>("Usuario añadido correctamente", HttpStatus.CREATED);
+	    } else {
+	        return new ResponseEntity<>("El usuario ya existe o hubo un error", HttpStatus.BAD_REQUEST);
+	    }
+		
+		// POSTMAN: localhost:8445/usuario/add
+				
+	}
+			
+		 
+
+	@DeleteMapping("/delete/{email}")
+	public ResponseEntity<?> eliminar(@PathVariable String email) {
+		
+		switch(usuarioService.deleteUno(email)) {
+			case 1:  return new ResponseEntity<>("Usuario eliminado correctamente", HttpStatus.OK);
+			case 0:  return new ResponseEntity<>("Usuario no existe", HttpStatus.NOT_FOUND);
+			case -1: return new ResponseEntity<>("Esto es un problema de la base de datos, llame a servicio Tecnico", HttpStatus.BAD_REQUEST);
+			default:  return new ResponseEntity<>("Error desconocido", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		// POSTMAN: localhost:8445/usuario/delete/nuevo@correo.com
+	}
 
 }
--- Reto_Grupo11/src/main/java/vacantes/modelo/services/UsuarioServiceImpl.java
+++ Reto_Grupo11/src/main/java/vacantes/modelo/services/UsuarioServiceImpl.java
@@ -1,40 +1,77 @@
 package vacantes.modelo.services;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import vacantes.modelo.entities.Usuario;
+import vacantes.repository.UsuarioRepository;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository uRepo;
+	
 
+	/**** CRUD ****/
 	@Override
-	public Usuario buscarUno(String clavePk) {
-		// TODO Auto-generated method stub
-		return null;
+	public Usuario buscarUno(String email) {
+		return uRepo.findById(email).orElse(null);
 	}
 
 	@Override
 	public List<Usuario> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		return uRepo.findAll();
 	}
 
 	@Override
-	public Usuario insertUno(Usuario entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Usuario insertUno(Usuario usuario) {
+		try {
+			if (uRepo.existsById(usuario.getEmail())) {
+				return null; // Me aseguro
+				// Si lo encuentra devuelve nulo para no darlo de alta.
+			}
+			else 
+				return uRepo.save(usuario);
+			
+		} catch (Exception e) {
+			e.printStackTrace(); // para las pruebas hago un syso de todo lo ocurrido.
+			return null;
+		}
 	}
 
 	@Override
-	public int updateUno(Usuario entidad) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int updateUno(Usuario usuario) {
+	    try {
+	        if (uRepo.existsById(usuario.getEmail())) {
+	            System.out.println("Actualizando usuario con Nombre: " + usuario.getNombre());
+	            uRepo.save(usuario);
+	            return 1; // Éxito
+	        } else {
+	            return 0; // Usuario no encontrado
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return -1; // Error en la base de datos
+	    }	
 	}
 
 	@Override
-	public int deleteUno(String clavePk) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int deleteUno(String email) {
+	    try {
+			if (uRepo.existsById(email)) {
+				uRepo.deleteById(email); // no devuele nada
+				return 1; // Si lo encuenta, y lo borra, devuelvo 1.
+			}
+			else 
+				return 0; // Si no existe, devuelvo 0
+				
+		} catch (Exception e) {
+			e.printStackTrace(); // para las pruebas hago un syso de todo lo ocurrido.
+			return -1; // Si se casca, devuelvo -1
+		}
 	}
+
 
 }
